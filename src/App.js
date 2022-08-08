e5be3a367b68ee94e2fc3bb8dfa7de1590b314ca
Ben Johnson <ben@bnjn.tech>
2022-08-08 03:34:40 +0100

    create and read added to api and frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+      getTodos();
+  }, []);
+
+  function getTodos() {
+      fetch('http://localhost:1337/tasks', {
+          method: 'GET',
+          mode: 'cors',
+      }).then((res) => {
+          return res.json();
+      })
+          .then((data) => {
+              setTodos(data);
+          })
+          .catch(err => console.error(err));
+  }
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      const title = document.querySelector('#taskTitle').value;
+      const taskBody = document.querySelector('#taskBody').value;
+      const task = {
+          title: title,
+          task: taskBody,
+          completed: false,
+          date: new Date()
+      };
+      createTask(task);
+  }
+
+  function createTask(task) {
+      fetch('http://localhost:1337/tasks', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          },
+          body: JSON.stringify(task)
+      }).then((res) => {getTodos()})
+        .catch(err => console.error(err));
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <div className='title'>
+                <h1>ToDo Application</h1>
+            </div>
+        </header>
+        <form className='task-form'>
+            <label form='taskTitle'>Title</label>
+            <input id='taskTitle' type='text'/>
+            <label form='taskBody'>Task</label>
+            <input id='taskBody' type='text'/>
+            <button id='submitButton' onClick={handleSubmit}>Submit</button>
+        </form>
+        <div className="tasks">
+        {
+            todos.map((todo, index) => {
+                console.log(todo.completed)
+                return (
+                    <div className='task-card' id={todo._id} key={index}>
+                        <h3>{todo.title}</h3>
+                        <p>{todo.task}</p>
+                        <p>Completed: {todo.completed ? 'yes' : 'no'}</p>
+                        <p>{todo.date}</p>
+                        <button>Edit</button><button>Delete</button>
+                    </div>
+                )
+            })
+        }
+        </div>
     </div>
   );
 }
---
+++ srv/server.js
@@ -0,0 +1,50 @@
+// Express
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'todoapp';
+
+MongoClient.connect(url, { useNewUrlParser: true })
+    .then((client) => {
+        // DB Setup
+        const db = client.db(dbName);
+        console.log(`Connected MongoDB`);
+        console.log(`Database: ${dbName}`);
+        const todosCollection = db.collection('todos');
+
+        // Middleware
+        app.use(bodyParser.json());
+        app.use(cors({
+            origin: 'http://localhost:3000'
+        }));
+
+        // Start server
+        app.listen(1337, () => {
+            console.log('listening on port 1337');
+        });
+
+        // GET routes
+        app.get('/', (req, res) => {
+            res.send('Todo backend up and running.');
+        });
+        app.get('/tasks', (req, res) => {
+            todosCollection.find().toArray()
+                .then(results => {
+                    res.status(200).send(results);
+                })
+                .catch(error => console.error(error))
+        });
+
+        // POST routes
+        app.post('/tasks',(req, res) => {
+            todosCollection.insertOne(req.body)
+                .then(result => {res.status(200).send()})
+                .catch(err => console.error(err))
+        });
+    })
+    .catch(console.error);
